24b9bdc6f4446ab5559f15de0902e12cea488b9d
tomachito <tomwillmorris@gmail.com>
2022-01-06 22:28:07 -0500

    test

--- scheduler.py
+++ scheduler.py
@@ -15,7 +15,7 @@ parser.add_argument('--tag', type=str, help='Email subject prefix', default='Poe
 parser.add_argument('--hour', type=str, help='Hour of the day to send', default=7)
 
 args = parser.parse_args()
-@#schedule.scheduled_job('cron', day_of_week='mon,tue,wed,thu,fri,sat,sun', hour=args.hour)
+#@schedule.scheduled_job('cron', day_of_week='mon,tue,wed,thu,fri,sat,sun', hour=args.hour)
 @schedule.scheduled_job('interval', minutes=1/6, job_defaults={'max_instances': 8})
 def send_daily_poem():
     print(f'This job is run every day at {args.hour}:00.')
