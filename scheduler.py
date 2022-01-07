1359d798ab39ee066db2e0fc9c63c3adb318f1cd
tomachito <tomwillmorris@gmail.com>
2022-01-06 22:30:33 -0500

    blah

--- scheduler.py
+++ scheduler.py
@@ -16,7 +16,7 @@ parser.add_argument('--hour', type=str, help='Hour of the day to send', default=
 
 args = parser.parse_args()
 #@schedule.scheduled_job('cron', day_of_week='mon,tue,wed,thu,fri,sat,sun', hour=args.hour)
-@schedule.scheduled_job('interval', minutes=1/6, job_defaults={'max_instances': 8})
+@schedule.scheduled_job('interval', minutes=1/6)
 def send_daily_poem():
     print(f'This job is run every day at {args.hour}:00.')
     with open(args.address,'r+') as f:
