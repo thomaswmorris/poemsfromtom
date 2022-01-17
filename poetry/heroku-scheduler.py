e662801e0d0e6200012a4060b4fb3d5b95dc3163
tomachito <tomwillmorris@gmail.com>
2022-01-17 10:46:16 -0500

    heroku stuff

--- poetry/heroku-scheduler.py
+++ poetry/heroku-scheduler.py
@@ -12,13 +12,13 @@ schedule = BlockingScheduler(timezone='America/New_York')
 import argparse, sys
 parser = argparse.ArgumentParser()
 parser.add_argument('--username', type=str, help='Email address from which to send the poem',default='')
-parser.add_argument('--password', type=str, help='Email password',default='')
+#parser.add_argument('--password', type=str, help='Email password',default='')
 parser.add_argument('--recipient', type=str, help='Where to send the poem',default='poemsfromtom@gmail.com')
 parser.add_argument('--repo_lsfn', type=str, help='Where to send the poem',default='')
 parser.add_argument('--poet', type=str, help='Which poet to send', default='random')
 parser.add_argument('--title', type=str, help='Which title to send', default='random')
 parser.add_argument('--repo', type=str, help='Which GH repository to load', default='')
-parser.add_argument('--token', type=str, help='GH token', default='')
+#parser.add_argument('--token', type=str, help='GH token', default='')
 parser.add_argument('--context', type=bool, help='Whether to send contextual poems', default=False)
 parser.add_argument('--rh', type=bool, help='Whether to consider past poems sent', default=False)
 parser.add_argument('--wh', type=bool, help='Whether to consider this poem in the future', default=False)
@@ -26,9 +26,6 @@ parser.add_argument('--hist_tag', type=str, help='What tag to write to the histo
 parser.add_argument('--subj_tag', type=str, help='Email subject prefix', default='')
 parser.add_argument('--hour', type=str, help='Hour of the day to send', default=7)
 args = parser.parse_args()
-
-
-print(os.environ['GITHUB_TOKEN'])
 
 def f(*arguments):
     done = False; fails = 0
@@ -43,19 +40,15 @@ def f(*arguments):
 
 @schedule.scheduled_job('cron', day_of_week='mon,tue,wed,thu,fri,sat,sun', hour=args.hour)
 def send_daily_poem():
+
     print(f'\nThis job is run every day at {args.hour} EST')
-    if '.csv' in args.address:
-        with open(args.address,'r+') as f:
-            entries = pd.read_csv(args.address,index_col=0)
-            #[entry for entry in f.read().split('\n') if len(entry) > 0]
-    else: entries = pd.DataFrame(columns=['name','email']); entries.loc[0] = '*', args.address
 
     # Choose a poem that meets the supplied conditions
     poetizer.load_poem(
         poet=args.poet, 
         title=args.title, 
         repo_name=args.repo,
-        repo_token=args.token,
+        repo_token=os.environ['GITHUB_TOKEN'],
         when=time.time(), 
         min_length=10, 
         max_length=2000, 
@@ -78,7 +71,7 @@ def send_daily_poem():
 
     for name, email in zip(entries['name'],entries['email']):
 
-        p = Process(target=f, args=(args.username, args.password, email, args.subj_tag))
+        p = Process(target=f, args=(args.username, os.environ['PFT_PW'], email, args.subj_tag))
         p.start()
         p.join()
 
