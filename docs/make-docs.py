1b9a5c2db1537607d1bcb393c18c01669f54f081
tomachito <tomwillmorris@gmail.com>
2022-01-30 17:53:04 -0500

    pages

--- docs/make-docs.py
+++ docs/make-docs.py
@@ -1,6 +1,6 @@
 
 
-
+from datetime import datetime
 import pandas as pd
 import sys
 sys.path.insert(1, 'poetry/')
@@ -16,16 +16,31 @@ poetizer = Poetizer()
 ys, ms, ds = [], [], []
 for loc in history.index:
 
-    y, m, d = history.loc[loc,'date'].split('-')
+    y, m, d = [int(x) for x in history.loc[loc,'date'].split('-')]
+
+    dt = datetime(y,m,d,7,0,0) 
+    dt_prev = datetime.fromtimestamp(dt.timestamp() - 86400)
+    dt_next = datetime.fromtimestamp(dt.timestamp() + 86400)
 
     if not os.path.isdir(f'docs/{y}'):         os.mkdir(f'docs/{y}')
     if not os.path.isdir(f'docs/{y}/{m}'):     os.mkdir(f'docs/{y}/{m}')
     if not os.path.isdir(f'docs/{y}/{m}/{d}'): os.mkdir(f'docs/{y}/{m}/{d}')
 
+
     poetizer.load_poem(poet=history.loc[loc,'poet'], title=history.loc[loc,'title'])
 
+
+    html = f'''
+    <html>
+        <h2 style="font-family:Garamond; color:Black; font-size: 24px; margin-bottom:0; margin : 0; padding-top:0;">
+        <a href="thomaswmorris.github.io/poetry/{dt_prev.year}/{dt_prev.month}/{dt_prev.day}">previous</a>
+        <a href="thomaswmorris.github.io/poetry/{dt_next.year}/{dt_next.month}/{dt_next.day}">next</a>
+        </h2>
+    </html>
+    '''
+
     with open(f'docs/{y}/{m}/{d}/index.html','w') as f:
-        f.write(poetizer.poem_html)
+        f.write(html + poetizer.poem_html)
 
 
 
