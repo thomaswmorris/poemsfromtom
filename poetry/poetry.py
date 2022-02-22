753cc7720c4a6fbf6089e9a9be018568547a4a27
tomachito <tomwillmorris@gmail.com>
2022-02-22 17:36:03 -0500

    a whole big thing

--- poetry/poetry.py
+++ poetry/poetry.py
@@ -104,7 +104,7 @@ class Poetizer:
             
             tag, name, birth, death, link = self.data[_poet]['metadata'].values()
             elapsed = (time.time() - self.history['timestamp'][self.history['poet']==_poet].max()) / 86400 # if _poet in self.history['poet'] else None
-            self.stats.loc[_poet] = name, birth, death, len(self.poems[_poet]), (self.history['poet']==_poet).sum(), np.round(elapsed,1)
+            self.stats.loc[_poet] = name, birth, death, len(self.data[_poet]['poems']), (self.history['poet']==_poet).sum(), np.round(elapsed,1)
             
         if not order_by is None:
             self.stats = self.stats.sort_values(by=order_by, ascending=ascending)
