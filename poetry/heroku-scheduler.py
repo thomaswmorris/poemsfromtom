ebe15f2d3efa34b61097c27deacb3eee3044adb3
tomachito <tomwillmorris@gmail.com>
2022-02-22 20:36:14 -0500

    tags

--- poetry/heroku-scheduler.py
+++ poetry/heroku-scheduler.py
@@ -37,7 +37,7 @@ def send_daily_poem():
     if args.type == 'test':
         when = datetime.fromtimestamp(ttime.time() + np.random.uniform(low=0,high=365) * 86400).timestamp()
         date, time = datetime.fromtimestamp(when).isoformat().split('T') 
-        subject = args.subj_tag + f' {date}:'
+        subject = args.subj_tag + f' {date}: '
     
     # Choose a poem that meets the supplied conditions
     poetizer.load_poem(
