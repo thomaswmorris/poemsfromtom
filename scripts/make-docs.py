6f1cf737a998650ee485083149b8badd5e8c48b1
Thomas Morris <tom@10-4-30-100.user.vpn.bnl.local>
2023-03-10 15:49:48 -0500

    weekly update 2022-03-12

--- scripts/make-docs.py
+++ scripts/make-docs.py
@@ -98,14 +98,15 @@ for i, entry in curator.history.iterrows():
 <head><!DOCTYPE html>
     <meta charset="utf-8">
     <title>{poem.nice_fancy_date}</title>
+    <link rel="icon" href="/assets/images/favicon.png">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="../../../assets/css/style.css">
+    <link rel="stylesheet" href="/assets/css/style.css">
     <style>
     {poems.css}
     </style>
 </head>
 <body>
-    <header style="background-image: url('../../../assets/images/bg/pissaro-pontoise.jpeg')"></header>
+    <header style="background-image: url('/assets/images/bg/pissaro-pontoise.jpeg')"></header>
     <nav>
         <ul>
             <li class="nav-item left"><a class="nav-link" href="/">home</a></li>
