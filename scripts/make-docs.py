e0a67f19d8b270fa0c570f6f1c20b6441e924409
Thomas Morris <thomasmorris@princeton.edu>
2023-04-18 20:54:18 -0400

    update 2023-04-18

--- scripts/make-docs.py
+++ scripts/make-docs.py
@@ -88,7 +88,7 @@ for i, entry in curator.history.iterrows():
 
     html = f'''<!DOCTYPE html>
 <html lang="en">
-<head><!DOCTYPE html>
+<head>
     <meta charset="utf-8">
     <title>{poem.nice_fancy_date}</title>
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
