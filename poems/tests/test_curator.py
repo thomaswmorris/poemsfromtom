3faa29b1b0db1bf1495ef074c088745f6c85d32f
Thomas Morris <thomasmorris@princeton.edu>
2024-07-27 21:56:22 -0400

    added jacobsen

--- poems/tests/test_curator.py
+++ poems/tests/test_curator.py
@@ -7,3 +7,9 @@ def test_dates():
     for author in ["bob-dylan", "t-s-eliot", "dante-alighieri", "horace", "ovid", "sappho", "anonymous"]:
         poem = curator.get_poem(author=author, verbose=False)
         print(f'{author:>16}: {repr(poem.author.dates(month_and_day=True))} or {repr(poem.author.dates(month_and_day=False))}')
+
+def test_poems():
+    curator = Curator()
+    for index, entry in curator.catalog.df.iterrows():
+        p = curator.get_poem(author=entry.author, title=entry.title)
+        p.email_html
